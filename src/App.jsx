c699a6d596a603da2dbe36a8168a2475007229f6
KODIYARASU01 <kodiyarasu02gmail.com>
2024-02-26 06:33:52 +0000

    Digital card show in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,58 @@
 import "./App.css";
-import Register from "./components/Register";
-import Reset from "./components/Reset";
-import Recovery from "./components/Recovery";
-import Profile from "./components/Profile";
-import Password from "./components/Password";
+// import Register from "./components/Register";
+// import Reset from "./components/Reset";
+// import Recovery from "./components/Recovery";
+// import Profile from "./components/Profile";
+// import Password from "./components/Password";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Admin from "./components/Admin/Admin";
-import Login from "./components/Login";
-import UserProfile from "./components/Profile/Profile";
-import Sidebar from "./components/Profile/Sidebar";
+// import Admin from "./components/Admin/Admin";
+// import Login from "./components/Login";
+// import UserProfile from "./components/Profile/Profile";
+// import Sidebar from "./components/Profile/Sidebar";
 import DigitalCard from "./components/Profile/DigitalCard";
 let router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <Login />,
+  // },
+  // {
+  //   path: "/register",
+  //   element: <Register />,
+  // },
+  // {
+  //   path: "/reset",
+  //   element: <Reset />,
+  // },
+  // {
+  //   path: "/recovery",
+  //   element: <Recovery />,
+  // },
+  // {
+  //   path: "/password",
+  //   element: <Password />,
+  // },
+
+  // {
+  //   path: "/profile",
+  //   element: <Profile />,
+  // },
+  // {
+  //   path: "/user-profile",
+  //   element: <UserProfile />,
+  // },
+  // {
+  //   path: "/sidebar",
+  //   element: <Sidebar />,
+  // },
   {
     path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/reset",
-    element: <Reset />,
-  },
-  {
-    path: "/recovery",
-    element: <Recovery />,
-  },
-  {
-    path: "/password",
-    element: <Password />,
-  },
-
-  {
-    path: "/profile",
-    element: <Profile />,
-  },
-  {
-    path: "/user-profile",
-    element: <UserProfile />,
-  },
-  {
-    path: "/sidebar",
-    element: <Sidebar />,
-  },
-  {
-    path: "/digitalCard",
     element: <DigitalCard />,
   },
-  {
-    path: "/admin",
-    element: <Admin />,
-  },
+  // {
+  //   path: "/admin",
+  //   element: <Admin />,s
+  // },
 ]);
 const App = () => {
   return (
